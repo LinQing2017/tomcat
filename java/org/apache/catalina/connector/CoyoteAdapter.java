83dfff790e1d5b4ec915161f0f539d4633245ca1
LinQing <m15651620957@163.com>
2021-02-09 21:10:30 +0800

    featrue: 修改sdp-app-name为sdp-m-name

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -675,8 +675,8 @@ public class CoyoteAdapter implements Adapter {
 
         // Request mapping.
         MessageBytes serverName;
-        if (req.getMimeHeaders().getValue("sdp-app-name") != null) {
-            serverName = req.getMimeHeaders().getValue("sdp-app-name");
+        if (req.getMimeHeaders().getValue("sdp-m-name") != null) {
+            serverName = req.getMimeHeaders().getValue("sdp-m-name");
         }else if ( connector.getUseIPVHosts()) {
             serverName = req.localName();
             if (serverName.isNull()) {
